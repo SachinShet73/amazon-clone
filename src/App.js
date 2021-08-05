e239adb02976b927589d5b5c2fe09e0008a2c543
SachinShet73 <sachinshet135@gmail.com>
2021-08-05 23:49:57 +0530

    removed invisible margin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello Programmers lets build amazon store</h1>
     </div>
   );
 }
