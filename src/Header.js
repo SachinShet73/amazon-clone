419affff147c855c626bee3dae5a65ca764eaeac
SachinShet73 <sachinshet135@gmail.com>
2021-08-06 01:49:43 +0530

    added styling for header option and header nav

--- src/Header.js
+++ src/Header.js
@@ -19,7 +19,7 @@ function Header() {
             <div className="header__nav">
                 <div className="header__option">
                     <span
-                        className='header__optionLineOne'>HELLO GUEST</span>
+                        className='header__optionLineOne'>Hello Guest</span>
                     <span
                         className='header__optionLineTwo'>Sign In</span>
 
