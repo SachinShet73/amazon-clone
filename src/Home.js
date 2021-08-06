723aa4ff3f9d0d252b439e289eb2920a00b14f1b
SachinShet73 <sachinshet135@gmail.com>
2021-08-06 23:24:14 +0530

    home page is now in development

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,12 @@ import "./Home.css";
 function Home() {
     return (
         <div className='home'>
-
+            <div className="home__container">
+                
+            </div>
         </div>
     )
 }
 
 export default Home;
+
