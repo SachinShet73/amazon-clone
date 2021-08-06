ff51cbb30330ee5db9af761a0500744f36740d22
SachinShet73 <sachinshet135@gmail.com>
2021-08-06 22:11:17 +0530

    started bto build Home page

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 function Header() {
     return (
         <div className="header">
@@ -36,6 +37,11 @@ function Header() {
                     <span
                         className='header__optionLineTwo'>Prime</span>
 
+                </div>
+
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header_optionLineTwo header__basketCount">0</span>
                 </div>
             </div>
         </div>
