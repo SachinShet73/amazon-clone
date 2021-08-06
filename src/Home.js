143ecbb8a8dc130fd2ac5149fa5ec01120dd4798
SachinShet73 <sachinshet135@gmail.com>
2021-08-06 23:59:09 +0530

    added product rows

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,23 @@ function Home() {
                 src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_TallHero_Gamers_en_US_1x._CB667161802_.jpg"
                 alt=""
                 />
+
+                <div className="home__row">
+                    {/*product*/}
+                    {/*product*/}
+                </div>
+
+                <div className="home__row">
+                    {/*product*/}
+                    {/*product*/}
+                    {/*product*/}
+                </div>
+                
+                <div className="home__row">
+                    {/*product*/}
+                    {/*product*/}
+                </div>
+
             </div>
         </div>
     )
