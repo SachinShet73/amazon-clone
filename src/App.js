648b58b0331f7b84125f2955d637d2b628af750c
SachinShet73 <sachinshet135@gmail.com>
2021-08-07 02:07:10 +0530

    added react router for navigation between different pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 function App() {
   return (
     // BEM
-    <div className="App">
-
+    <Router>
+      <div className="App">
       <Header />
       <Home />
-      {/* Home */}
-    </div>
+      </div>
+    </Router>
+    
   );
 }
 
