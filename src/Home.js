ad26eb035705443ba1755fb4cd845bdd3cb8e8f0
SachinShet73 <sachinshet135@gmail.com>
2021-08-06 23:28:24 +0530

    added the home image

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,11 @@ function Home() {
     return (
         <div className='home'>
             <div className="home__container">
-                
+                <img 
+                className = "home__image"
+                src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_TallHero_Gamers_en_US_1x._CB667161802_.jpg"
+                alt=""
+                />
             </div>
         </div>
     )
