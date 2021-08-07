abf462c335a1c9025a8b862762ccf221116adb64
SachinShet73 <sachinshet135@gmail.com>
2021-08-07 23:11:08 +0530

    added a link to shopping basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Checkout.css';
+import './Subtotal.js'
+import Subtotal from './Subtotal';
 function Checkout() {
     return (
     <div className = "checkout">
@@ -25,7 +27,9 @@ function Checkout() {
         <div className ="checkout__right">
         
 
-        <h2>The subtotal will go here</h2>
+      <Subtotal /> 
+
+
 
 
         </div>
