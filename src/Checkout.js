2c544f1c1c995db78dc37106b8451c78fecf7c4a
SachinShet73 <sachinshet135@gmail.com>
2021-08-13 01:19:04 +0530

    styling

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,37 +6,47 @@ import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './Stateprovider';
 import FlipMove from 'react-flip-move';
 function Checkout() {
-    const [{basket , user},dispatch]= useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
     return (
-    <div className = "checkout">
-       
-        <div className ="checkout__left">
+        
+            <div className="checkout">
 
-            <img className = "checkout__ad" 
-            src = "https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/AUGART21/Event_Page_Header_EN_PC.jpg " 
-            alt = "">
-            </img>
-            <div>
-                <h3>Hello, {user ? user.email : "Guest"} </h3>
-                <h2 className = "checkout__title">
-                    Your shopping basket
-                </h2>
-{basket.map(item => (
-    <CheckoutProduct
-    id={item.id}
-    title={item.title}
-    image={item.image}
-    price={item.price}
-    rating={item.rating}
-    />
-))} 
+                <div className="checkout__left">
+
+                    <img className="checkout__ad"
+                        src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/AUGART21/Event_Page_Header_EN_PC.jpg "
+                        alt="">
+                    </img>
+                    <div>
+                        <h3>Hello, {user ? user.email : "Guest"} </h3>
+                        <h2 className="checkout__title">
+                            Your shopping basket
+                        </h2>
+
+                      
+
+                            {basket.map(item => (
+                                <CheckoutProduct
+                                    id={item.id}
+                                    title={item.title}
+                                    image={item.image}
+                                    price={item.price}
+                                    rating={item.rating}
+                                />
+                            ))}
+
+                        
+
+
+                    </div>
+                </div>
+                <div className="checkout__right">
+                    <Subtotal />
+                </div>
             </div>
-        </div>
-        <div className ="checkout__right">
-      <Subtotal /> 
-     </div>
-    </div>
+        
     )
+
 }
 
 export default Checkout
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './CheckoutProduct.css'
 import { useStateValue } from './Stateprovider';
+import FlipMove from 'react-flip-move';
 
-function CheckoutProduct({id,image,title,price,rating}) {
+function CheckoutProduct({ id, image, title, price, rating }) {
 
-    const [{basket},dispatch] = useStateValue();
-    const removeFromBasket = ()  => 
-    {
+    const [{ basket }, dispatch] = useStateValue();
+    const removeFromBasket = () => {
         //remove the item form the basket
 
         dispatch({
@@ -16,32 +16,33 @@ function CheckoutProduct({id,image,title,price,rating}) {
     }
     return (
         
-        <div className = 'checkoutProduct'>
-            <img className = 'checkoutProduct__image' src = {image} alt = "" />
+            <div className='checkoutProduct'>
+                <img className='checkoutProduct__image' src={image} alt="" />
 
-             <div className = "checkoutProduct__info">
-                 <p className = "checkoutProduct_title">{title}</p>
+                <div className="checkoutProduct__info">
+                    <p className="checkoutProduct_title">{title}</p>
 
-                 <p className="checkoutProduct__price"> <small>₹</small>
-                 <strong>{price}</strong></p>
+                    <p className="checkoutProduct__price"> <small>₹</small>
+                        <strong>{price}</strong></p>
 
-                 <div className = "checkoutProduct__rating">
-                     {
-                         Array(rating)
-                         .fill()
-                         .map((_,i) =>(
-                             <p>⭐️</p>
-                         ))
-                     }
-                 </div>
+                    <div className="checkoutProduct__rating">
+                        {
+                            Array(rating)
+                                .fill()
+                                .map((_, i) => (
+                                    <p>⭐️</p>
+                                ))
+                        }
+                    </div>
 
-                 <button onClick = {removeFromBasket}>
-                     Remove from basket
-                 </button>
+                    <button onClick={removeFromBasket}>
+                        Remove from basket
+                    </button>
 
-                
-             </div>
-        </div>
+
+                </div>
+            </div>
+        
     )
 }
 
