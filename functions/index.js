079d086d372f941b175623dafd00f71b8fddd478
SachinShet73 <sachinshet135@gmail.com>
2021-08-22 23:39:17 +0530

    backend

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const stripe = require("stripe")
+('sk_test_51JOjzLSGxTQrEUyuVc3PnR5HQHUfyx9rp8X8F2TuFwqBokYGsHUqTTMloPoQph26HyFuhv5I7JpMRcQ9IsyrG7C400I9QRoDi1')
+
+//api
+//app config
+
+const app = express();
+
+//middlewares
+app.use(cors({origin: true}))
+app.use(express.json())
+
+//API routes
+app.get('/',(request , response) => response.status(200).send('HELLO WORLD'))
+//-Listen Command
+
+exports.api = functions.https.onRequest(app)
